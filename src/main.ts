a1fd0ad30f7481ae2327903ab914878a4c9eb491
josip-volarevic <josip.volarevic4@gmail.com>
2022-09-03 16:52:14 +0200

    Project Init

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('☀️ Helius MTF API')
+    .setDescription("API endpoints for 'Mint the Flag' challenge")
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
+  app.enableCors();
+  await app.listen(process.env.PORT || 3005);
+}
+
+bootstrap();
